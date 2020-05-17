32c12ad2a2fd40c2453e1a802057faea19cbe806
Calvin <naws_9217@outlook.com>
2020-05-18 00:55:09 +0800

    Visitor prototype::Liu Haowen and Ye Zhouguo

---
+++ backend/ASTVisitor/src/myASTVisitor.cpp
@@ -0,0 +1,145 @@
+//------------------------------------------------------------------------------
+// Clang rewriter sample. Demonstrates:
+//
+// * How to use RecursiveASTVisitor to find interesting AST nodes.
+// * How to use the Rewriter API to rewrite the source code.
+//
+// Eli Bendersky (eliben@gmail.com)
+// This code is in the public domain
+//------------------------------------------------------------------------------
+#include <cstdio>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/ParseAST.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Rewrite/Frontend/Rewriters.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang;
+using namespace std;
+
+SourceManager *SM;
+
+// By implementing RecursiveASTVisitor, we can specify which AST nodes
+// we're interested in by overriding relevant methods.
+class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor>
+{
+public:
+  MyASTVisitor(Rewriter &R) : TheRewriter(R) {}
+
+  bool VisitStmt(Stmt *s)
+  {
+    // Only care about For statements.
+    if (isa<ForStmt>(s))
+    {
+      ForStmt *ForStatement = cast<ForStmt>(s);
+      SourceLocation beginLoc = ForStatement->getBeginLoc();
+      string locString = beginLoc.printToString(*SM);
+      printf("for loop detected::%s\n", locString.c_str());
+    }
+    else if (isa<DeclStmt>(s))
+    {
+      DeclStmt* ds=cast<DeclStmt>(s);
+      SourceLocation beginLoc = ds->getBeginLoc();
+      Decl* dcl=ds->getDeclGroup().getSingleDecl();
+      if(isa<VarDecl>(dcl))
+      {
+        VarDecl* vd=cast<VarDecl>(dcl);
+        QualType qt=vd->getType();
+        std::string qtgas=qt.getAsString();
+        if(qtgas.find('*')!=qtgas.npos)
+        {
+          string locString = beginLoc.printToString(*SM);
+          printf("pointer decl detected::%s\n", locString.c_str());
+        }
+      }
+    }
+    return true;
+  }
+
+private:
+  Rewriter &TheRewriter;
+};
+
+class MyASTConsumer : public ASTConsumer
+{
+public:
+  MyASTConsumer(Rewriter &R) : Visitor(R) {}
+
+  virtual bool HandleTopLevelDecl(DeclGroupRef DR)
+  {
+    for (DeclGroupRef::iterator b = DR.begin(), e = DR.end(); b != e; ++b)
+      // Traverse the declaration using my AST visitor.
+      Visitor.TraverseDecl(*b);
+    return true;
+  }
+
+private:
+  MyASTVisitor Visitor;
+};
+
+int main(int argc, char *argv[])
+{
+
+  if (argc != 2)
+  {
+    llvm::errs() << "Usage: myASTVisitor <filename>\n";
+    return 1;
+  }
+
+  CompilerInstance TheCompInst;
+  TheCompInst.createDiagnostics();
+
+  LangOptions &lo = TheCompInst.getLangOpts();
+  lo.CPlusPlus = 1;
+
+  auto TO = std::make_shared<TargetOptions>();
+  TO->Triple = llvm::sys::getDefaultTargetTriple();
+  TargetInfo *TI =
+      TargetInfo::CreateTargetInfo(TheCompInst.getDiagnostics(), TO);
+  TheCompInst.setTarget(TI);
+
+  TheCompInst.createFileManager();
+  FileManager &FileMgr = TheCompInst.getFileManager();
+  TheCompInst.createSourceManager(FileMgr);
+  SourceManager &SourceMgr = TheCompInst.getSourceManager();
+  SM = &SourceMgr;
+  TheCompInst.createPreprocessor(TU_Module);
+  TheCompInst.createASTContext();
+
+  Rewriter TheRewriter;
+
+  TheRewriter.setSourceMgr(SourceMgr, TheCompInst.getLangOpts());
+
+  // Set the main file
+  const FileEntry *FileIn = FileMgr.getFile(argv[1]);
+  SourceMgr.setMainFileID(
+      SourceMgr.createFileID(FileIn, SourceLocation(), SrcMgr::C_User));
+  TheCompInst.getDiagnosticClient().BeginSourceFile(
+      TheCompInst.getLangOpts(), &TheCompInst.getPreprocessor());
+
+  MyASTConsumer TheConsumer(TheRewriter);
+
+  // Parse the file to AST, registering our consumer as the AST consumer.
+  ParseAST(TheCompInst.getPreprocessor(), &TheConsumer,
+           TheCompInst.getASTContext());
+
+  // const RewriteBuffer *RewriteBuf =
+  //     TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
+  // llvm::outs() << std::string(RewriteBuf->begin(), RewriteBuf->end());
+
+  return 0;
+}
