ba8a16a659fc111f9aee4bce8f11fde7a33125a1
yezhouguo <565676178@qq.com>
2020-05-19 01:05:39 +0800

    SlowMemoryOperationDemo

--- backend/testcases/Tmemory.cpp
+++ backend/testcases/Tmemory.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #define mc memcpy
 int main()
 {
-    int a[10000];
-    int b[10000];
-    for(int i=0;i<10000;i++) a[i]=rand();
-    //
-    mc(b,a,10000*sizeof(int));
+    char a[10000];
+    unsigned char b[10000];
+    for(int i=0;i<10000;i++)
+        a[i]='0';
+    mc(b,a,10000*sizeof(char));
     return 0;
 }
