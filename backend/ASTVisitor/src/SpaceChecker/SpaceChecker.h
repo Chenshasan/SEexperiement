fcd64351a035110d552721e8a34f2c6637a8a598
yezhouguo <565676178@qq.com>
2020-07-06 19:23:35 +0800

    .

--- backend/ASTVisitor/src/SpaceChecker/SpaceChecker.h
+++ backend/ASTVisitor/src/SpaceChecker/SpaceChecker.h
@@ -1,28 +1,53 @@
 #ifndef __SPACECHECKER_H__
 #define __SPACECHECKER_H__
 
+#include <string>
+#include <vector>
+
 #include "clang/AST/Decl.h"
 
-#include "../common/printer.h"
+#include "../Common/printer.h"
 
-#define WARNING_TRIGGER_VARIABLE_SIZE (1*1024*1024*8)
+#define WARNING_TRIGGER_VARIABLE_SIZE (1 * 1024 * 1024 * 8)
 
 using namespace clang;
 using namespace std;
 
-class SpaceChecker: public printer
+struct Field
+{
+
+    string type;
+
+    string name;
+
+    string location;
+
+    bool isUsed;
+
+    Field(string t, string n, string l, bool u) : type(t),
+                                                  name(n),
+                                                  location(l),
+                                                  isUsed(u)
+    {
+    }
+};
+
+class SpaceChecker : public Printer
 {
 
 public:
-
     SpaceChecker();
 
-    SpaceChecker(const SpaceChecker& bvc) = delete;
+    SpaceChecker(const SpaceChecker &bvc) = delete;
 
-    void bigVariableCheck(VarDecl* vd);
+    void bigVariableCheck(VarDecl *vd);
 
+    void bigFieldCheck(FieldDecl *fd);
+
+    static void unusedFieldCheck();
+
+private:
+    double bitToMb(double bits);
 };
-
-double bitToMb(double bits);
 
 #endif
